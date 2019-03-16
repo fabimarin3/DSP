05675d1f2f677fa08a7538c6bb05e21a72c04725
fabimarin3 <fabimr3596@gmail.com>
2019-03-16 13:09:09 -0600

    Figuras

--- Ejercicio_02/Ejercicio_02.m
+++ Ejercicio_02/Ejercicio_02.m
@@ -12,13 +12,14 @@
 % ylabel('xp(t)');%Nombre del eje y
 % title('Coseno en función del tiempo');%Nombre del eje x
 
+clear all;
+close all
+clc;
 
 t1=0:0.001:2;%Defino t2 para que sea de 0 a 2, este va  en pasos de 1
 x1= exp(cos((40*pi*t1)/(9)));%Función que quiero graficar, en términos del tiempo t2
 n=0:36; %Defino el intervalo de la función
 x2 = exp(cos((40*pi*n)/(9))); %Defino la función
-
-clear 
 
 figure(2);
 subplot(2,1,1);
