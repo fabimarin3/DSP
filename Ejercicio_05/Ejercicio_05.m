744685a5d024856498bf23b13063f396b692472f
fabimarin3 <fabimr3596@gmail.com>
2019-03-16 14:08:21 -0600

    Cambio en ejercicio 5

--- Ejercicio_05/Ejercicio_05.m
+++ Ejercicio_05/Ejercicio_05.m
@@ -4,15 +4,7 @@
 
 close all
 
-sT(10,500,4000,1,0,0.01);
-xlabel('Tiempo')
-ylabel('s(t)')
-title('Función s continua')
-hold on 
-sN(10,500,4000,1,0,0.01);
-xlabel('Muestras')
-ylabel('s(n)')
-title('Función s discreta')
+s(10,500,4000,1,0,0.01);
 
 
 
--- Ejercicio_06/Ejercicio_06.m
+++ Ejercicio_06/Ejercicio_06.m
@@ -2,23 +2,19 @@
 %%%%Fabiola Marín Rivera%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Am=5; %Amplitud de la función, solamente varía la amplitd, no el tiempo ni la frecuencia del tono
-fs=2000;  % Frecuencia de muestreo, varia el paso del muestreo de función por ende el tono cambia 
-duration=5; %Me dice hasta cuanto quiero llegar los valores de muestreo
-freq=2500; %Frecuencia de la función, varia la forma de la función, el tono 
-values=0:1/fs:duration; %Valores para los cuales deseo hacer el muestreo
-a=Am*sin(2*pi* freq*values); %Función que quiero hacer sonar
-sound(a); %Comando para hacer sonar a 'a'
-figure(1)
-stem(a);%Grafica de 'a' en función de los valores de muestreo
-axis([0 25 0 10]) %Acomodo de los ejes
-xlabel('Frecuencia (Hz)')
-ylabel('Función a(n)')
-title('Ejercicio 06')
+function x = tono(frecuencia)
 
-figure(2)
-plot(a);%Grafica de 'a' en función de los valores de muestreo
-axis([0 25 0 10]) %Acomodo de los ejes
-xlabel('Frecuencia (Hz)')
-ylabel('Función a(f)')
-title('Ejercicio 06')
+f = frecuencia
+
+while f < 22000
+
+    fs=44000;  % sampling frequency
+    duration=1;
+    values=0:1/fs:duration;
+    a=cos(2*pi*f*values);
+    sound(a,44000);
+    pause(duration);
+    f = f + frecuencia;
+
+end 
+
