4e14db6dd11f4e8772b30cdfdcf623091d2aefa8
fabimarin3 <fabimr3596@gmail.com>
2019-04-05 20:52:40 -0600

    Update Ejercicio_01.m
    Cambios

--- Tarea 2/Ejercicio 01/Ejercicio_01.m
+++ Tarea 2/Ejercicio 01/Ejercicio_01.m
@@ -1,3 +1,45 @@
-%Ejercicio #1
+%Ejercicio 1
 %Fabiola Marín Rivera
+
+close all
+clc
+clf
+
+
+step  = 2^-7;
+v_fix = fi(0.5:step:(2-step), 1, 20); % fixed-point inputs in range [.5, 2)
+niter = 10; % number of CORDIC iterations
+x_sqr = cordicsqrt(v_fix, niter);
+
+% Get the Real World Value (RWV) of the CORDIC outputs for comparison
+% and plot the error between the MATLAB reference and CORDIC sqrt values
+x_cdc = double(x_sqr); % CORDIC results (scaled by An_hp)
+v_ref = double(v_fix); % Reference floating-point input values
+x_ref = sqrt(v_ref);   % MATLAB reference floating-point results
+figure;
+subplot(211);
+plot(v_ref, x_cdc, 'r.', v_ref, x_ref, 'b-');
+legend('CORDIC', 'Reference', 'Location', 'SouthEast');
+title('CORDIC Square Root (In-Range) and MATLAB Reference Results');
+subplot(212);
+absErr = abs(x_ref - x_cdc);
+plot(v_ref, absErr);
+title('Absolute Error (vs. MATLAB SQRT Reference Results)');
+
+
+
+
+
+%%%%%%%%%%%%%Referencias%%%%%%%%%%%%%%%%
+% https://la.mathworks.com/help/fixedpoint/examples/compute-square-root-using-cordic.html
+
+
+
+
+
+
+
+
+
+
 
